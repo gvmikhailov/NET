aa84d63cb9e19d0ed0e58ae5dc0354fdfe05ff8f
gvmikhailov <sad-gn@yandex.ru>
2018-03-30 20:35:47 +0300

    Nineth Commit
    9

---
+++ Current/SummInteger/SummInteger/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// http://acm.timus.ru/problem.aspx?space=1&num=1068
+
+namespace SummInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int summ = 0;
+            int n = rnd.Next(-10000, 10001);
+            if (n <= 1)
+            {
+                for (int i = n; i <= 1; i++)
+                    summ += i;
+            }
+            else
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    summ += j;
+                }
+            }
+            Console.WriteLine($"Сумма всех целых чисел, лежащих между 1 и {n} включительно - {summ}");
+        }
+    }
+}
