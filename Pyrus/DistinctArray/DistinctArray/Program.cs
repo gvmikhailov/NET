200d1faabf51536476d022eccae58a696512865b
gvmikhailov <sad-gn@yandex.ru>
2018-04-03 10:15:54 +0300

    16-th Commit
    16

--- Pyrus/DistinctArray/DistinctArray/Program.cs
+++ Pyrus/DistinctArray/DistinctArray/Program.cs
@@ -13,7 +13,7 @@ namespace DistinctArray
             Random rnd = new Random();
             int srcArraySize = rnd.Next(10, 31);
             int[] src = new int[srcArraySize];
-            Console.WriteLine("Создан массив из {0} элементов:");
+            Console.WriteLine("Создан массив из {0} элементов:",srcArraySize);
             for (int i = 0; i < src.Length; i++)
             {
                 src[i] = rnd.Next(1, 10);
