028d5f9ec3036d2134ac103323d35290187589c3
gvmikhailov <sad-gn@yandex.ru>
2018-04-04 12:29:32 +0300

    17-th Commit
    17

---
+++ belstu.by/Employee/Employee/Employee.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Employee
+    {
+        private int _id;
+        private double _salary;
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    _id = 0;
+                }
+                else
+                {
+                    _id = value;
+                }
+            }
+        }
+        public double Salary
+        {
+            get
+            {
+                return _salary;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    _salary = 0;
+                }
+                else
+                {
+                    _salary = Math.Round(value, 2);
+                }
+            }
+        }
+
+        public Employee(int id, double salary)
+        {
+            Id = id;
+            Salary = salary;
+        }
+
+        internal void PrintInfo()
+        {
+            Console.WriteLine($"Сотрудник с табельным номером {Id} имеет оклад {Salary}");
+        }
+    }
+}
