f17ede7f262bf8e914540e29704eec0ae12bf4f2
gvmikhailov <sad-gn@yandex.ru>
2018-10-28 11:55:27 +0300

    41-th Commit

---
+++ Algorythmical Exercises/Numbers/AlarmClock/AlarmClock/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace AlarmClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime alarmTime = new DateTime(2018, 10, 28, 11, 27, 00);
+            string alarmTimeString = $"{alarmTime:g}";
+            while(true)
+            {
+                string nowString = $"{DateTime.Now:g}";
+                if(alarmTimeString == nowString)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Пора вставать!");
+                    PlaySound();
+                    break;
+                }
+                else
+                {
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    string substract = alarmTime.Subtract(DateTime.Now).ToString();
+                    string leftTime = substract.Remove(substract.Length - 8);
+                    Console.WriteLine($"До будильника осталось: {leftTime}");
+                    continue;
+                }
+            }
+            
+        }
+
+        static void PlaySound()
+        {
+            MediaPlayer player = new MediaPlayer();
+            player.Open(new Uri("C:\\Program Files (x86)\\GTA Vice City\\Audio\\bike1_3.wav", UriKind.Absolute));
+            player.Play();
+            Thread.Sleep(4000);
+        }
+    }
+}
