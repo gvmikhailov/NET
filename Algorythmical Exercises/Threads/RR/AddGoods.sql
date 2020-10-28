57e728adefb50ef1c599cab0d4f91e5a3e361068
gvmikhailov <35683342+gvmikhailov@users.noreply.github.com>
2020-10-28 23:03:52 +0300

    RR2

--- Algorythmical Exercises/Threads/RR/AddGoods.sql
+++ Algorythmical Exercises/Threads/RR/AddGoods.sql
@@ -2,26 +2,26 @@ use Test
 go
 
 create procedure dbo.AddGoods
-	 @XML		 xml
-	,@ShopID varchar(256)
+   @XML		 xml
+  ,@ShopID   varchar(256)
 as
 begin
-	declare @AllGoods	table (
-		 [GoodBarCode]		bigint					not null
-		,[ShopId]					varchar(256)		not null
-		,[GoodName]				varchar(8000)		not null
-	)
+  declare @AllGoods	table (
+     [GoodBarCode]	bigint			not null
+    ,[ShopId]		varchar(256)	not null
+    ,[GoodName]		varchar(8000)	not null
+  )
 
-	insert into @AllGoods (
-		 [GoodBarCode]
-		,[ShopId]
-		,[GoodName]
-	)
-	select distinct
-			 f.value('@barcode','bigint')																									as [GoodBarCode]
-      ,@ShopID																																			as [ShopId]
-      ,concat(f.value('@author','varchar(512)'), f.value('@name','varchar(7424)'))  as [GoodName]
-		from @XML.nodes('offers/offer') as t(f)
+  insert into @AllGoods (
+     [GoodBarCode]
+    ,[ShopId]
+    ,[GoodName]
+  )
+  select distinct
+  	   f.value('@barcode','bigint')																									as [GoodBarCode]
+  	  ,@ShopID																																			as [ShopId]
+  	  ,concat(f.value('@author','varchar(512)'), f.value('@name','varchar(7424)'))  as [GoodName]
+    from @XML.nodes('offers/offer') as t(f)
 
   -----------------------------
   begin tran Update_Data
@@ -29,28 +29,28 @@ begin
     merge dbo.Good as trg
       using (
         select
-						 g.[GoodBarCode]
-						,g.[ShopId]
-						,g.[GoodName]
+		     g.[GoodBarCode]
+		    ,g.[ShopId]
+		    ,g.[GoodName]
           from @AllGoods as g
       ) as src on src.[GoodBarCode] = trg.[GoodBarCode]
-							and src.[ShopId] = trg.[ShopId]
+			  and src.[ShopId] = trg.[ShopId]
    
     when matched and src.[GoodName] <> trg.[GoodName] then
       update set
-           trg.[GoodName] = src.[GoodName]
+          trg.[GoodName] = src.[GoodName]
     when not matched then 
       insert (
          [GoodBarCode]
         ,[ShopId]
         ,[GoodName]
       ) values (
-           src.[GoodBarCode]
-          ,src.[ShopId]
-          ,src.[GoodName]
+         src.[GoodBarCode]
+        ,src.[ShopId]
+        ,src.[GoodName]
       )
-		when not matched by source then
-			delete
+	when not matched by source then
+	  delete
     ;    
   -----------------------------
   commit tran Update_Data
