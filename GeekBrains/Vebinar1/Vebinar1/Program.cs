47835dc779e1f8f35fbc2c13dd3417fd4c1e02b3
gvmikhailov <35683342+gvmikhailov@users.noreply.github.com>
2020-12-10 08:37:49 +0300

    GBLes1-1
    Дз по первому вебинару

---
+++ GeekBrains/Vebinar1/Vebinar1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Vebinar1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте,\nвведите, пожалуйста свое имя и нажмите Enter.");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now.ToLongDateString()}");
+            Console.ReadKey();
+        }
+    }
+}
