877ec95edf8f0032cb41a2742088e4473d4a0dbf
gvmikhailov <35683342+gvmikhailov@users.noreply.github.com>
2021-01-31 12:35:30 +0300

    GB2Les5-1

---
+++ GeekBrains/Algorythms/Vebinar5/BinaryTree/BinaryTree.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryTree
+{
+    class BinaryTree
+    {
+        public BinaryTree Parent { get; set; }
+        public BinaryTree Left { get; set; }
+        public BinaryTree Right { get; set; }
+        public BinaryTree Root { get; set; }
+        public int Value { get; set; }
+
+        public BinaryTree(int val, BinaryTree parent)
+        {
+            Value = val;
+            Parent = parent;
+        }
+
+        public void AddValue(int val)
+        {
+            if (val.CompareTo(Value) < 0)
+            {
+                if (Left == null)
+                    Left = new BinaryTree(val, Left);
+                else if (Left != null)
+                    Left.AddValue(val);
+            }
+            else
+            {
+                if (Right == null)
+                    Right = new BinaryTree(val, Right);
+                else if (Right != null)
+                    Right.AddValue(val);
+            }
+        }
+
+        public BinaryTree SearchByBFS(int value)
+        {
+            Queue<BinaryTree> searchQueue = new Queue<BinaryTree>();
+            BinaryTree current = Root;
+            searchQueue.Enqueue(Root);
+
+            while(searchQueue.Count != 0)
+            {
+                current = searchQueue.Dequeue();
+                Console.WriteLine($"Берется элемент со значением {current.Value} и сравнивается с числом {value}");
+                if (current.Value == value)
+                {
+                    Console.WriteLine("Это необходимый элемент дерева");
+                    return current;
+                }
+                else
+                {
+                    if (current.Left != null)
+                        searchQueue.Enqueue(current.Left);
+                    if (current.Right != null)
+                        searchQueue.Enqueue(current.Right);
+                }
+            }
+
+            Console.WriteLine($"Элемент со значением {value} не найден в дереве!");
+            return null;
+        }
+
+        public BinaryTree SearchByDFS(int value)
+        {
+            Stack<BinaryTree> searchStack = new Stack<BinaryTree>();
+            BinaryTree current = Root;
+            searchStack.Push(Root);
+
+            while (searchStack.Count != 0)
+            {
+                current = searchStack.Pop();
+                Console.WriteLine($"Берется элемент со значением {current.Value} и сравнивается с числом {value}");
+                if (current.Value == value)
+                {
+                    Console.WriteLine("Это необходимый элемент дерева");
+                    return current;
+                }
+                else
+                {
+                    if (current.Right != null)
+                        searchStack.Push(current.Right);
+                    if (current.Left != null)
+                        searchStack.Push(current.Left);
+                }
+            }
+
+            Console.WriteLine($"Элемент со значением {value} не найден в дереве!");
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
---
+++ GeekBrains/Algorythms/Vebinar5/BinaryTree/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rList = new Random(47);
+
+            int maxVal = 1000;
+            int n = 20;
+
+            List<int> list = new List<int>();
+            list.Add(rList.Next(maxVal));
+            BinaryTree tree = new BinaryTree(maxVal, null);
+            tree.Root = tree;
+
+            for (int i = 0; i < n; i++)
+            {
+                int val = rList.Next(maxVal);
+                if (!list.Contains(val))
+                {
+                    list.Add(val);
+                    tree.AddValue(val);
+                }
+            }
+
+            BinaryTreePrinter.Print(tree.Root);
+
+            tree.SearchByBFS(200);
+            tree.SearchByDFS(200);
+
+            Console.ReadKey();
+        }
+    }
+}
