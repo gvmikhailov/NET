17545c6d0ecaf4a43a007bb7c893d03d22678974
gvmikhailov <35683342+gvmikhailov@users.noreply.github.com>
2021-02-06 19:57:47 +0300

    GB2Les6-1

---
+++ GeekBrains/Algorythms/Vebinar6/Vebinar6/Graph.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Vebinar6
+{
+    class Graph
+    {
+        public Dictionary<int, HashSet<int>> Nodes { get; set; }
+
+        public Graph()
+        {
+            Nodes = new Dictionary<int, HashSet<int>>();
+        }
+
+        public void AddToEnd(int source, int target)
+        {
+            if (Nodes.ContainsKey(source))
+            {
+                try
+                {
+                    Nodes[source].Add(target);
+                }
+                catch
+                {
+                    Console.WriteLine("Данный путь уже существует: " + source + " к " + target);
+                }
+            }
+            else
+            {
+                var hs = new HashSet<int>();
+                hs.Add(target);
+                Nodes.Add(source, hs);
+            }
+        }
+
+        public int? BFSFind(int element)
+        {
+            int start = 1; // идем всегда от первой ноды
+            HashSet<int> visited = new HashSet<int>();
+            visited.Add(start);
+            Queue<int> find = new Queue<int>();
+            find.Enqueue(start); 
+
+            while (find.Count > 0)
+            {
+                int current = find.Dequeue();
+                Console.Write($"Берем ноду со значением {current} - ");
+                if (current == element)
+                {
+                    Console.WriteLine("И это она!");
+                    return visited.Count();
+                }
+                else
+                {
+                    Console.WriteLine("И это не она");
+                }
+
+                if (Nodes.ContainsKey(current))
+                {
+                    foreach (int neighbour in Nodes[current].Where(a => !visited.Contains(a)))
+                    {
+                        visited.Add(neighbour);
+                        find.Enqueue(neighbour);
+                    }
+                }
+            }
+            Console.WriteLine("Нода с таким значением не найдена!");
+            return null;
+        }
+
+        public int? DFSFind(int element)
+        {
+            int start = 1; // идем всегда от первой ноды
+            HashSet<int> visited = new HashSet<int>();
+            visited.Add(start);
+            Stack<int> find = new Stack<int>();
+            find.Push(start);
+
+            while (find.Count > 0)
+            {
+                int current = find.Pop();
+                Console.Write($"Берем ноду со значением {current} - ");
+                if (current == element)
+                {
+                    Console.WriteLine("И это она!");
+                    return visited.Count();
+                }
+                else
+                {
+                    Console.WriteLine("И это не она");
+                }
+
+                if (Nodes.ContainsKey(current))
+                {
+                    foreach (int neighbour in Nodes[current].Where(a => !visited.Contains(a)))
+                    {
+                        visited.Add(neighbour);
+                        find.Push(neighbour);
+                    }
+                }
+            }
+            Console.WriteLine("Нода с таким значением не найдена!");
+            return null;
+        }
+
+    }
+}
---
+++ GeekBrains/Algorythms/Vebinar6/Vebinar6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Vebinar6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph graph = new Graph();
+            graph.AddToEnd(1, 2);
+            graph.AddToEnd(1, 3);
+            graph.AddToEnd(1, 4);
+            graph.AddToEnd(2, 5);
+            graph.AddToEnd(2, 6);
+            graph.AddToEnd(4, 7);
+            graph.AddToEnd(4, 8);
+            graph.AddToEnd(5, 9);
+            graph.AddToEnd(5, 10);
+            graph.AddToEnd(7, 11);
+            graph.AddToEnd(7, 12);
+
+            int? bfs = graph.BFSFind(7);
+            int? dfs = graph.DFSFind(4);
+
+            Console.ReadKey();
+        }
+    }
+}
