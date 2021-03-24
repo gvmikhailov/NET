74de31338c44cd08632926ceebbb096aaf643d48
gvmikhailov <35683342+gvmikhailov@users.noreply.github.com>
2021-03-24 17:38:32 +0300

    GB3Les1-1

---
+++ GeekBrains/ASPNetCore/WeatherAPI/WeatherAPI/Model/WeatherMetrics.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace WeatherAPI.Model
+{
+    public class WeatherMetrics
+    {
+        public List<WeatherData> TemperatureData { get; set; }
+
+        public WeatherMetrics()
+        {
+            TemperatureData = new List<WeatherData>();
+        }
+    }
+}
