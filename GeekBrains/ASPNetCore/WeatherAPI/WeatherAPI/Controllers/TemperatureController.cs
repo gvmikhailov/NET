dc915b63e4091082e65b6a653c4d8cb8f202042a
gvmikhailov <35683342+gvmikhailov@users.noreply.github.com>
2021-03-29 10:18:25 +0300

    @gvmikhailov GB3Les1-5

--- GeekBrains/ASPNetCore/WeatherAPI/WeatherAPI/Controllers/TemperatureController.cs
+++ GeekBrains/ASPNetCore/WeatherAPI/WeatherAPI/Controllers/TemperatureController.cs
@@ -18,50 +18,50 @@ namespace WeatherAPI.Controllers
         }
 
         [HttpGet("read")]
-        public IEnumerable<WeatherData> Get([FromQuery] DateTime dateFrom, [FromQuery] DateTime dateTo)
+        public IEnumerable<WeatherData> Get([FromBody] DateScope scope)
         {
-            return _holder.TemperatureData.Where(i => i.Date >= dateFrom && i.Date <= dateTo).ToArray();
+            return _holder.TemperatureData.Where(i => i.Date >= scope.DateFrom && i.Date <= scope.DateTo).ToArray();
         }
 
         [HttpPost("set")]
-        public string Set([FromBody] WeatherData data)
+        public IActionResult Set([FromBody] WeatherData data)
         {
             if (!_holder.TemperatureData.Contains(data))
             {
                 _holder.TemperatureData.Add(data);
-                return "Значение успешно добавлено";
+                return OK();
             }
             else
             {
-                return "Неподдерживаемый метод!";
+                return NotFound();
             }
         }
 
-        [HttpPost("update")]
-        public string Update([FromBody] WeatherData data)
+        [HttpPut("update")]
+        public IActionResult Update([FromBody] WeatherData data)
         {
             if (_holder.TemperatureData.Contains(data))
             {
                 int elem = _holder.TemperatureData.IndexOf(data);
                 _holder.TemperatureData[elem].Date = data.Date;
                 _holder.TemperatureData[elem].Temperature = data.Temperature;
-                return "Значение успешно обновлено";
+                return OK();
             }
             else
             {
-                return "Невозможно изменить элемент, так как его не существует!";
+                return NotFound();
             }
         }
 
-        [HttpPost("delete")]
-        public string Delete([FromBody] DateScope scope)
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromBody] DateScope scope)
         {
             List<WeatherData> toDelete = _holder.TemperatureData.Where(i => i.Date >= scope.DateFrom && i.Date <= scope.DateTo).ToList();
             foreach (var t in toDelete)
             {
                 _holder.TemperatureData.Remove(t);
             }
-            return "Данные успешно удалены!";
+            return OK();
         }
     }
 }
